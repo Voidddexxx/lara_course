5c9aaac7fd24809b9686b4f0ca0357a0eecf5f2b
Sergey Kamenev <dexvoid@yandex.ru>
2025-11-09 22:11:56 +0700

    feat: create project

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\SampleController;
+use \App\Http\Controllers\PostsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SampleController::class, 'home']);
+
+Route::resource('/posts', PostsController::class);
+
+//Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
+//Route::get('/posts/:post', [PostsController::class, 'show'])->name('posts.show');
